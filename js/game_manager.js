0a33b823901c42d803a173815c3a7b57b9da57fa
AlCodes <awwebber18@gmail.com>
2014-09-24 13:46:33 -0500

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,10 +1,10 @@
 function GameManager(size, InputManager, Actuator, StorageManager) {
-  this.size           = size; // Size of the grid
+  this.size           = 10; // Size of the grid
   this.inputManager   = new InputManager;
   this.storageManager = new StorageManager;
   this.actuator       = new Actuator;
 
-  this.startTiles     = 2;
+  this.startTiles     = 16;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
